8565508333d8c299888e46118c2163d882ff9890
aureliendu917 <general@aur3l.fr>
2017-01-15 00:41:54 +0100

    .

--- app/Controllers/SeriesController.php
+++ app/Controllers/SeriesController.php
@@ -42,7 +42,7 @@ class SeriesController extends Controller {
     $data['listseasons'] = json_decode($this->container->sickrage->showSeasons($args['tvdbId']));
     $data['tvdb'] = $this->ReverseArray($this->container->tvdb->getSerieEpisodes($args['tvdbId'], "fr"));
     $data['config'] = $this->container->configdefault;
-    echo"<pre>"; print_r($this->container->tvdb->getBannersFiltered($args['tvdbId'], "poster")[0]['path']); echo"</pre>"; die();
+    echo"<pre>"; print_r($this->container->tvdb->getBannersFiltered($args['tvdbId'], "poster")); echo"</pre>"; die();
     $data['series']['banner'] = $this->multiRezise($this->container->tvdb->getBanners($args['tvdbId']), $args['tvdbId'], "tmp/covers",['small']);
     foreach ($data['tvdb']['episodes'] as $numS => $season) {
       foreach ($season as $numE => $ep) {
