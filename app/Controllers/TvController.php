3ce6e255b5f42fc11527af883b9839e5c9a2a27b
aureliendu917 <general@aur3l.fr>
2017-01-18 22:00:11 +0100

    Add poster

--- app/Controllers/TvController.php
+++ app/Controllers/TvController.php
@@ -32,8 +32,11 @@ class TvController extends Controller {
         $data['seasons'][$numS][$numE]['info'] = $episode;
       }
     }
+    $poster = $this->container->tvdb->getBannersFiltered($id, "poster");
+    $nameposter = "http://thetvdb.com/banners/".$poster[0]->path;
+    $data['banner'] = $this->multiRezise($nameposter, $id, "tmp/covers",['small']);
+    $data['palette'] = ColorThief::getPalette($nameposter, 2);
 
-    $this->getArray($data);
     $this->render($response, 'tv/tv.twig',$data);
   }
 
