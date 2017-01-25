21fbeca880a2671aa96a3c1b5e0cfaa2a3998fb9
aureliendu917 <general@aur3l.fr>
2017-01-25 19:33:33 +0100

    .

--- app/Controllers/TvController.php
+++ app/Controllers/TvController.php
@@ -32,7 +32,7 @@ class TvController extends Controller {
         $thumbnailLien = $img->crop(100, 100, 25, 25);
         $dir = dirname(dirname(__DIR__));
         $url = $dir.'/public/tmp/covers/'.$key.'.jpg';
-        $urlNo = '/public/tmp/covers/'.$key.'.jpg';
+        $urlNo = '/tmp/covers/'.$key.'.jpg';
         $img->save($url);
         $thumbnailLien = $urlNo;
         if(fopen($thumbnailLien, "r")){
